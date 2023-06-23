76f0c6e3868c83bdc0c30282a9598c32f3911546
Dmitry <Ivanovdmitry530@gmail.com>
2023-06-23 11:38:17 +0400

    Добавьте файлы проекта.

---
+++ Tic_Tac_Toe_board/Tic_Tac_Toe_board.cpp
@@ -0,0 +1,36 @@
+﻿// Программа Tic_Tac_Toe_board
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int ROWS = 3;
+    const int COLUMNS = 3;
+    char board[ROWS][COLUMNS] = { {'0','X','0'}, // инициализируем массив 3x3 и все элементы массива
+    {' ', 'X', 'X'},
+    {'X', '0', '0'} };
+    cout << "Here's the tic-tac-toe board: \n";
+    for (int i = 0; i < ROWS; ++i) 
+    {
+        for (int j = 0; j < COLUMNS; ++j)
+        {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+    cout << "\n'X' moves to the empty location.\n\n";
+    board[1][0] = 'X';
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+    cout << "\n'X' wins!";
+        return 0;
+}
+
+
